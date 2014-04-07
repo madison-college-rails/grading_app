568acd53ca3b3262af7620a5a891d8f11e509c91
Mark McEahern <mark@mceahern.com>
2014-04-07 17:45:10 -0500

    rails new grading_app --skip-test-unit --database postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GradingApp::Application.config.secret_key_base = '052963f217e8c0d946b40af2582453e902c93a72b075d31e79062dea2b14749e3e35347e61224ebea326b99988362eb944b041bd5502ff4aae7e8dbb52e1515c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GradingApp::Application.config.session_store :cookie_store, key: '_grading_app_session'
