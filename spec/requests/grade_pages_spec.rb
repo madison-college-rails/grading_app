e311dd7e52736f57bfd44c195fcd6c272eb8a0cf
Mark McEahern <mark@mceahern.com>
2014-04-07 19:10:39 -0500

    Add system exam to spec

--- spec/requests/grade_pages_spec.rb
+++ spec/requests/grade_pages_spec.rb
@@ -1,17 +1,19 @@
 require 'spec_helper'
 
 describe "Exams" do
+  before { create :exam, system: true, answers: "1 C\n2 B"}
+
   describe 'can be created' do
     before { visit root_path }
 
-    let(:exam) { build :exam }
+    let(:exam) { build :exam, answers: "1 A\n2 B" }
 
-    it 'has a create button' do
+    it 'and are graded upon submission' do
       fill_in 'Student name', with: exam.student_name
       fill_in 'Answers', with: exam.answers
       click_button 'Create Exam'
 
-      expect(page).to have_content 'Your grade is'
+      expect(page).to have_content 'Your grade is 50.0'
     end
   end
 end
